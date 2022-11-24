ea44bb2a89c88950825432c3bdfb92e980df3c5e
Андрей <97339527+rarinaferrari@users.noreply.github.com>
2022-11-24 12:00:16 +0300

    Update passman.py

--- passman.py
+++ passman.py
@@ -1,5 +1,11 @@
-#! /bin/env python
+#!/bin/env python
 
+
+a,b = input(), input()
+import base64
 from sjcl import SJCL
-cyphertext = SJCL().encrypt(b"udyeaxor", "admin")
+cyphertext = SJCL().encrypt(a.encode('utf-8'), b.encode('utf-8'))
+#s = str(cyphertext)
+#s = s.encode("UTF-8")
+cyphertext = base64.b64encode(str(cyphertext).encode('utf-8')).decode('utf-8')
 print (cyphertext)
